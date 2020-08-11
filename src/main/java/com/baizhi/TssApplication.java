e9639a59ad9be654cb15efe3ab7a885cf85232b9
jingfei <jingfei@bxtdata.com>
2020-08-11 16:20:29 +0800

    修改; 了注释

--- src/main/java/com/baizhi/TssApplication.java
+++ src/main/java/com/baizhi/TssApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TssApplication {
 
     public static void main(String[] args) {
-//    	×¢ÊÍ    adsfasdf 
+//    å¤§å“¥    adsfasdf 
         SpringApplication.run(TssApplication.class, args);
     }
 
